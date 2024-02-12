1fb8431cf260e7a37088bd0d694ca275d73f2f2d
yoiner100 <viveiguaranureche@gmail.com>
2024-02-12 16:41:11 -0500

    realizando la pagina de blog

--- Desktop/ecomerce/src/app/app.component.ts
+++ Desktop/ecomerce/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
-import { BlogComponent } from './pages/blog/blog.component';
-import { HomeComponent } from './pages/home/home.component';
+
 
 @Component({
   selector: 'app-root',
--- Desktop/ecomerce/src/app/pages/home/home.component.ts
+++ Desktop/ecomerce/src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component} from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { HeaderComponent } from '../../components/header/header.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ HeaderComponent,FooterComponent ],
+  imports: [ FooterComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
