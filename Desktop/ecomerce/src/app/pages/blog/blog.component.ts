c7f10bf64adcb83ad74cc19d7ab921c9bfd44cc3
yoiner100 <viveiguaranureche@gmail.com>
2024-02-12 23:10:50 -0500

    terminando la pagina blog

--- Desktop/ecomerce/src/app/pages/blog/blog.component.ts
+++ Desktop/ecomerce/src/app/pages/blog/blog.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [ FooterComponent ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
